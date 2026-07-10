ratel-ai/ratel-mcp

feat: add daemon http server

apps/ratel-mcp/src/cli/handlers/serve.ts
@@ -35,6 +35,11 @@ export interface ServeResult {
   shutdown: () => Promise<void>;
 }
 
+export interface ConfiguredGateway {
+  config: ReturnType<typeof mergeConfigs>;
+  gateway: Awaited<ReturnType<typeof buildGatewayFromConfig>>;
+}
+
 export interface AutoConfigResolution {
   configPaths: string[];
   projectRoot?: string;
@@ -46,32 +51,7 @@ export async function runServe(
   options: ServeOptions,
   log: (m: string) => void,
 ): Promise<ServeResult> {
-  const autoConfig = booleanFlag(parsed.flags["auto-config"]);
-  if (autoConfig && parsed.configPaths.length > 0) {
-    throw new Error("ratel-mcp serve: --auto-config cannot be combined with --config paths");
-  }
-  if (!autoConfig && parsed.configPaths.length === 0) {
-    throw new Error("usage: ratel-mcp serve <config.json> [--config <path> ...]");
-  }
-
-  const readConfig = options.readConfig ?? defaultReadConfig;
-  const configPaths = autoConfig
-    ? resolveAutoConfig(parsed, options, log).configPaths
-    : parsed.configPaths;
-  const parts = [];
-  for (const p of configPaths) {
-    const raw = await readConfig(p);
-    parts.push(parseConfig(raw));
-  }
-  const config = mergeConfigs(parts);
-
-  const trace = await resolveTraceSink(parsed, log);
-
-  const gateway = await buildGatewayFromConfig(config, {
-    transportFactory: options.transportFactory,
-    logger: log,
-    ...(trace ? { trace } : {}),
-  });
+  const { config, gateway } = await buildConfiguredGateway(parsed, options, log);
 
   const downstream = options.serverTransport ?? new StdioServerTransport();
   const exposed = await createMcpServer(gateway.catalog, {
@@ -93,6 +73,42 @@ export async function runServe(
       await gateway.close();
     },
   };
+}
+
+export async function buildConfiguredGateway(
+  parsed: ParsedArgs,
+  options: ServeOptions,
+  log: (m: string) => void,
+): Promise<ConfiguredGateway> {
+  const command = parsed.group;
+  const autoConfig = booleanFlag(parsed.flags["auto-config"]);
+  if (autoConfig && parsed.configPaths.length > 0) {
+    throw new Error(`ratel-mcp ${command}: --auto-config cannot be combined with --config paths`);
+  }
+  if (!autoConfig && parsed.configPaths.length === 0) {
+    throw new Error(`usage: ratel-mcp ${command} <config.json> [--config <path> ...]`);
+  }
+
+  const readConfig = options.readConfig ?? defaultReadConfig;
+  const configPaths = autoConfig
+    ? resolveAutoConfig(parsed, options, log).configPaths
+    : parsed.configPaths;
+  const parts = [];
+  for (const p of configPaths) {
+    const raw = await readConfig(p);
+    parts.push(parseConfig(raw));
+  }
+  const config = mergeConfigs(parts);
+
+  const trace = await resolveTraceSink(parsed, log);
+
+  const gateway = await buildGatewayFromConfig(config, {
+    transportFactory: options.transportFactory,
+    logger: log,
+    ...(trace ? { trace } : {}),
+  });
+
+  return { config, gateway };
 }
 
 export function resolveAutoConfig(

ratel-ai/ratel-local

apps/ratel-local/src/cli/handlers/serve.ts
@@ -35,6 +35,11 @@ export interface ServeResult {
   shutdown: () => Promise<void>;
 }
 
+export interface ConfiguredGateway {
+  config: ReturnType<typeof mergeConfigs>;
+  gateway: Awaited<ReturnType<typeof buildGatewayFromConfig>>;
+}
+
 export interface AutoConfigResolution {
   configPaths: string[];
   projectRoot?: string;
@@ -46,32 +51,7 @@ export async function runServe(
   options: ServeOptions,
   log: (m: string) => void,
 ): Promise<ServeResult> {
-  const autoConfig = booleanFlag(parsed.flags["auto-config"]);
-  if (autoConfig && parsed.configPaths.length > 0) {
-    throw new Error("ratel-local serve: --auto-config cannot be combined with --config paths");
-  }
-  if (!autoConfig && parsed.configPaths.length === 0) {
-    throw new Error("usage: ratel-local serve <config.json> [--config <path> ...]");
-  }
-
-  const readConfig = options.readConfig ?? defaultReadConfig;
-  const configPaths = autoConfig
-    ? resolveAutoConfig(parsed, options, log).configPaths
-    : parsed.configPaths;
-  const parts = [];
-  for (const p of configPaths) {
-    const raw = await readConfig(p);
-    parts.push(parseConfig(raw));
-  }
-  const config = mergeConfigs(parts);
-
-  const trace = await resolveTraceSink(parsed, log);
-
-  const gateway = await buildGatewayFromConfig(config, {
-    transportFactory: options.transportFactory,
-    logger: log,
-    ...(trace ? { trace } : {}),
-  });
+  const { config, gateway } = await buildConfiguredGateway(parsed, options, log);
 
   const downstream = options.serverTransport ?? new StdioServerTransport();
   const exposed = await createMcpServer(gateway.catalog, {
@@ -93,6 +73,42 @@ export async function runServe(
       await gateway.close();
     },
   };
+}
+
+export async function buildConfiguredGateway(
+  parsed: ParsedArgs,
+  options: ServeOptions,
+  log: (m: string) => void,
+): Promise<ConfiguredGateway> {
+  const command = parsed.group;
+  const autoConfig = booleanFlag(parsed.flags["auto-config"]);
+  if (autoConfig && parsed.configPaths.length > 0) {
+    throw new Error(`ratel-local ${command}: --auto-config cannot be combined with --config paths`);
+  }
+  if (!autoConfig && parsed.configPaths.length === 0) {
+    throw new Error(`usage: ratel-local ${command} <config.json> [--config <path> ...]`);
+  }
+
+  const readConfig = options.readConfig ?? defaultReadConfig;
+  const configPaths = autoConfig
+    ? resolveAutoConfig(parsed, options, log).configPaths
+    : parsed.configPaths;
+  const parts = [];
+  for (const p of configPaths) {
+    const raw = await readConfig(p);
+    parts.push(parseConfig(raw));
+  }
+  const config = mergeConfigs(parts);
+
+  const trace = await resolveTraceSink(parsed, log);
+
+  const gateway = await buildGatewayFromConfig(config, {
+    transportFactory: options.transportFactory,
+    logger: log,
+    ...(trace ? { trace } : {}),
+  });
+
+  return { config, gateway };
 }
 
 export function resolveAutoConfig(